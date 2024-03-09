f30d21b6c4d8ff723fc6ee56593be874e234b20a
Ria Kulkarni <111171759+aluminates@users.noreply.github.com>
2024-03-09 14:42:16 +0530

    Update connection.js

--- backend/connection.js
+++ backend/connection.js
@@ -2,18 +2,11 @@ const mongoose = require('mongoose');
 // require('dotenv').config();
 
 mongoose.connect(
-  `mongodb+srv://Riya:Riya2002@cluster0.7jq2ytu.mongodb.net/chatAppMern?retryWrites=true&w=majority`,
+  `enter mongodb uri here`,
   (err) => {
    if(err) console.log(err) 
    else console.log("MongoDB connection established!");
   }
 );
 
-
-
-// mongosh "mongodb+srv://cluster0.7jq2ytu.mongodb.net/myFirstDatabase" --apiVersion 1 --username Riya
-
-// mongosh `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.7jq2ytu.mongodb.net/chatAppMern?retryWrites=true&w=majority" --apiVersion 1 --username Riya`
-
-// mongosh `mongodb+srv://Riya:Riya2002@cluster0.7jq2ytu.mongodb.net/chatAppMern?retryWrites=true&w=majority" --apiVersion 1 --username Riya`
 
